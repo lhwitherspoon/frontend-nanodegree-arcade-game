c87f163a45182bbb407f2da4309de4494de4b769
Les <les.witherspoon@gmail.com>
2017-06-10 23:19:12 -0700

    feat checkCollision function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -11,18 +11,11 @@ var Enemy = function(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    //setting up the bounding box area for tne bugs; this is rect1 in the algorthim from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-    var enemyHitBox = function(x, y) {
-        enemyHitBox.x = this.x;
-        enemyHitBox.y = this.y;
-        enemyHitBox.width = this.width;
-        enemyHitBox.height = this.height;
-    }
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -30,77 +23,81 @@ Enemy.prototype.update = function(dt) {
     if (this.x > 500) {
         this.x = -40;
     }
+    this.checkCollisions();
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
+
+};
+var checkCollisions = function(){
+    for(i = 0; i < allEnemies.length; i++){
+
+    //setting up the collision detection with the algorithm from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+
+    if (allEnemies[i].x < this.x + this.width &&
+        allEnemies[i].x + allEnemies[i].width > this.x &&
+        allEnemies[i].y < this.y + this.height &&
+        allEnemies[i].height + allEnemies[i].y > this.y) {
+        console.log("collision detected!");
+        this.x = 225;
+        this.y = 400;
+    }
+    }
 };
 
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, speed) {
+var Player = function (x, y, speed) {
     this.sprite = 'images/char-boy.png';
     this.width = 67; //setting a working width
     this.height = 88; //setting a working height
     this.x = x;
     this.y = y;
     this.speed = speed;
-    //setting up the bounding box area for the player; this is rect2 in the algorithm from         https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-
-
-    Player.prototype.update = function(dt) {
-        //this.handleInput();
-        if (this.x > 450) {
-            this.x = 425;
-        }
-        if (this.x < -15) {
-            this.x = 15;
-        }
-        if (this.y > 400) {
-            this.y = 425;
-        }
-        if (this.y < -15) {
-            this.y = 400;
-        }
-    };
-    Player.prototype.render = function() {
-        ctx.drawImage(Resources.get(this.sprite),
-            this.x, this.y);
-    };
-    Player.prototype.handleInput = function(allowedKeys) {
-        if (allowedKeys == 'left') {
-            this.x -= 20;
-        } else if (allowedKeys == 'right') {
-            this.x += 20;
-        } else if (allowedKeys == 'up') {
-            this.y -= 20;
-        } else {
-            this.y += 20;
-        }
-
-    };
-    Player.prototype.checkCollisions = function() {
-        var playerHitBox = {
-            this.x: Player.x,
-            this.y: Player.y,
-            this.width: Player.width,
-            this.height: Player.height,
-        }
-    };
-
-    //setting up the collision detection with the algorithm from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-
-    if (enemyHitBox.x < playerHitBox.x + playerHitBox.width &&
-        enemyHitBox.x + enemyHitBox.width > playerHitBox.x &&
-        enemyHitBox.y < playerHitBox.y + playerHitBox.height &&
-        enemyHitBox.height + playerHitBox.y > playerHitBox.y) {
-        console.log("collision detected!");
-    }
 };
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite),
+        this.x, this.y);
+
+};
+
+
+Player.prototype.update = function (dt) {
+    //this.handleInput();
+    if (this.x > 450) {
+        this.x = 425;
+    }
+    if (this.x < -15) {
+        this.x = 15;
+    }
+    if (this.y > 425) {
+        this.y = 400;
+    }
+    if (this.y < -15) {
+        this.y = 400;
+    }
+    //this.checkCollisions(allEnemies, player);
+
+};
+Player.prototype.handleInput = function (allowedKeys) {
+    if (allowedKeys == 'left') {
+        this.x -= 20;
+    } else if (allowedKeys == 'right') {
+        this.x += 20;
+    } else if (allowedKeys == 'up') {
+        this.y -= 20;
+    } else {
+        this.y += 20;
+    }
+
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -115,7 +112,7 @@ var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
