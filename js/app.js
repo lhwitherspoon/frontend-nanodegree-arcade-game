2729d2bad4085bc5074b7c6b6a087eb22b8c89eb
Les Witherspoon <leswitherspoon@gmail.com>
2017-06-10 21:12:13 -0700

    feat: random position and speed for bugs each time they re-enter the gameboard from the left.

--- js/app.js
+++ js/app.js
@@ -1,50 +1,56 @@
 // Enemies our player must avoid
-var Enemy = function (x, y, speed) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    //thanks to Karol's note at https://discussions.udacity.com/t/bugs-and-player-collision-is-occurring-too-early/242650/4 about reducing the width and height for Enemy and Player to make collisions occur when they are nearer to each other.
     this.sprite = 'images/enemy-bug.png';
-    this.width = 99; //setting a working width for now
-    this.height = 77; //setting a working height for now
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    this.width = 80; //setting a working width for now
+    this.height = 65; //setting a working height for now
+    //set random initial x & y coordinates for each enemy;
+    this.x = Math.floor(Math.random() * 11) + 15;
+    this.y = Math.floor(Math.random() * 301) + 50;
+
+    this.speed = Math.floor(Math.random() * 76) + 25;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
+Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x = this.x + this.speed * dt;
     if (this.x > 500) {
-        this.x = -40;
+        this.x = Math.floor(Math.random() * 11) + 15;
+        this.y = Math.floor(Math.random() * 201) + 100;
+        this.speed = Math.floor(Math.random() * 76) + 25;
+
     }
     this.checkCollisions();
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
+Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 
 };
-var checkCollisions = function(){
-    for(i = 0; i < allEnemies.length; i++){
+Enemy.prototype.checkCollisions = function() {
+
 
     //setting up the collision detection with the algorithm from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 
-    if (allEnemies[i].x < this.x + this.width &&
-        allEnemies[i].x + allEnemies[i].width > this.x &&
-        allEnemies[i].y < this.y + this.height &&
-        allEnemies[i].height + allEnemies[i].y > this.y) {
+    if (player.x < this.x + this.width &&
+        player.x + player.width > this.x &&
+        player.y < this.y + this.height &&
+        player.height + player.y > this.y) {
         console.log("collision detected!");
-        this.x = 225;
-        this.y = 400;
-    }
+        player.x = 225;
+        player.y = 400;
+
     }
 };
 
@@ -52,23 +58,17 @@ var checkCollisions = function(){
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function (x, y, speed) {
+var Player = function(x, y, speed) {
     this.sprite = 'images/char-boy.png';
-    this.width = 67; //setting a working width
-    this.height = 88; //setting a working height
+    this.width = 50; //setting a working width
+    this.height = 50; //setting a working height
     this.x = x;
     this.y = y;
     this.speed = speed;
 };
 
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite),
-        this.x, this.y);
 
-};
-
-
-Player.prototype.update = function (dt) {
+Player.prototype.update = function(dt) {
     //this.handleInput();
     if (this.x > 450) {
         this.x = 425;
@@ -80,20 +80,26 @@ Player.prototype.update = function (dt) {
         this.y = 400;
     }
     if (this.y < -15) {
+        this.x = 225;
         this.y = 400;
     }
-    //this.checkCollisions(allEnemies, player);
 
 };
-Player.prototype.handleInput = function (allowedKeys) {
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite),
+        this.x, this.y);
+
+};
+
+Player.prototype.handleInput = function(allowedKeys) {
     if (allowedKeys == 'left') {
-        this.x -= 20;
+        this.x -= 15;
     } else if (allowedKeys == 'right') {
-        this.x += 20;
+        this.x += 15;
     } else if (allowedKeys == 'up') {
-        this.y -= 20;
+        this.y -= 15;
     } else {
-        this.y += 20;
+        this.y += 15;
     }
 
 };
@@ -101,9 +107,9 @@ Player.prototype.handleInput = function (allowedKeys) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var bug1 = new Enemy(15, 100, 35);
-var bug2 = new Enemy(15, 200, 55);
-var bug3 = new Enemy(25, 300, 48);
+var bug1 = new Enemy(this.x, this.y, this.speed);
+var bug2 = new Enemy(this.x, this.y, this.speed);
+var bug3 = new Enemy(this.x, this.y, this.speed);
 
 var allEnemies = [bug1, bug2, bug3];
 
@@ -112,7 +118,7 @@ var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function (e) {
+document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
--- js/engine.js
+++ js/engine.js
@@ -80,7 +80,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-       // checkCollisions();
+        //checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -171,7 +171,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Star.png'
     ]);
     Resources.onReady(init);
 
